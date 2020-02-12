93e189a15b8ed092804879eaa693bc1a8c9ccc37
Xavier Wright <xmwrig79@harrisonburg.k12.va.us>
2020-02-12 08:26:53 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,27 +1,37 @@
-import pygame # import library
+
+import pygame
 pygame.init()
 
-# Create the window
 win = pygame.display.set_mode((800, 600))
+img = pygame.image.load('assets/forest-assets/door.png').convert()
+
+font = pygame.font.SysFont("arial", 72)
+# Create the text object
+text = font.render("Hello, world", True, (255, 255, 255))
+
+# Load the spritesheet
+spritesheet = pygame.image.load('assets/gfx/objects.png').convert()
+
+# Create the first image
+smol_img = pygame.Surface([16, 16]).convert()
+smol_img.blit(spritesheet, (0, 0), (0, 0, 16, 16))
+
+# Create the second image
+smolr_img = pygame.Surface([16, 16]).convert()
+smolr_img.blit(spritesheet, (0, 0), (16, 0, 16, 16))
 
 run = True
 while run:
-  for event in pygame.event.get():
-    if event.type == pygame.QUIT:
-      run = False
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            run = False
 
-# Game code starts here ---------------------
-  win.fill((0, 0, 0))
+    win.fill((0, 0, 0))
+    win.blit(img, (400, 300))
+    win.blit(smol_img, (100, 100))
+    win.blit(smolr_img, (150, 100))
 
-  # Draw a rectangle
-  pygame.draw.rect(win, (0, 204, 102), (50, 50, 100, 200))
-  
-  #Update the display
-  pygame.display.update()
 
-print("Ending game")
+    pygame.display.update()
+
 pygame.quit()
-
-pygame.image.load([ main.py ]).convert()
-
-img = pygame.image.load(' sprite.png').convert
